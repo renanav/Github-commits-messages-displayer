bbcc892c32cfb70de32d1146a72e125e088105bf
renanav <renan.avrahami@gmail.com>
2016-04-14 22:44:10 -0400

    Update files

--- challenge.rb
+++ challenge.rb
@@ -9,6 +9,8 @@ puts "What is the repository's name?"
 print "Repository's name: "
 repository_name=gets.chomp
 
-response = HTTParty.get("https://api.github.com/users/repos/#{username}/#{repository_name}/commits/sha")
-response = JSON.parse(page)
-puts page.class
+response = HTTParty.get("https://api.github.com/repos/#{username}/#{repository_name}/commits")
+body = JSON.parse response.body
+puts body.class
+puts body
+# puts response
